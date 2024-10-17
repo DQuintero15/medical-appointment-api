4ed1a1097f39b99f18916502c2426723404689f1
Daniel Quintero <quinterod164@gmail.com>
2024-10-17 13:53:10 -0500

    fix: update application yml

--- src/main/java/com/healthcare/bot/medical_appointment_api/MedicalAppointmentChatbotApiApplication.java
+++ src/main/java/com/healthcare/bot/medical_appointment_api/MedicalAppointmentChatbotApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:application.properties")
 public class MedicalAppointmentChatbotApiApplication {
 
 	public static void main(String[] args) {
