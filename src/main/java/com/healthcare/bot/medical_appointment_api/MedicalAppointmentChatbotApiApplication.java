1365e396c9e4d7c8152688bef9855574673d6c2a
Daniel Quintero <quinterod164@gmail.com>
2024-10-17 13:45:33 -0500

    chore: add application properties handling

--- src/main/java/com/healthcare/bot/medical_appointment_api/MedicalAppointmentChatbotApiApplication.java
+++ src/main/java/com/healthcare/bot/medical_appointment_api/MedicalAppointmentChatbotApiApplication.java
@@ -2,10 +2,12 @@ package com.healthcare.bot.medical_appointment_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource("classpath:application.properties")
 public class MedicalAppointmentChatbotApiApplication {
 
 	public static void main(String[] args) {
